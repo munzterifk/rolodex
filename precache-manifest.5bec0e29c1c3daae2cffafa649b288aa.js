0f7b5d5a60a57a48dcc16ecb662bec6d3da4089f
Noel Pastrana <noelpastranacastor@gmail.com>
2022-01-04 20:04:59 +0800

    Updates

---
+++ precache-manifest.5bec0e29c1c3daae2cffafa649b288aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2e9a01eaa612bc6b0c29790bdb52fb",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "f1a374cefc3a90eb9d8a",
+    "url": "/rolodex/static/css/main.11319235.chunk.css"
+  },
+  {
+    "revision": "2af87e7e7eb979cf8302",
+    "url": "/rolodex/static/js/2.2357b43c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/rolodex/static/js/2.2357b43c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a374cefc3a90eb9d8a",
+    "url": "/rolodex/static/js/main.50998d97.chunk.js"
+  },
+  {
+    "revision": "604800cde4d3fb0ab0cb",
+    "url": "/rolodex/static/js/runtime-main.b7003d66.js"
+  }
+]);
